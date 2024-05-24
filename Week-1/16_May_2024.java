353841b6b18246f02dc53218a098cc65bfed15da
Keerti Vishwkarma <138362568+keerti1924@users.noreply.github.com>
2024-05-24 14:09:07 +0530

    Add files via upload

---
+++ Week-1/16_May_2024.java
@@ -0,0 +1,6 @@
+class java{
+    public static void main(String args[]){
+        System.out.println("Hello World !");
+        System.out.println("My name is Keerti Vishwkarma.");
+    }
+}
---
+++ Week-1/17_May_2024.java
@@ -0,0 +1,46 @@
+class datatype{
+    public static void main(String args[]){
+        System.out.println("Hello World!");
+        System.out.println("My name is Keerti Vishwkarma.");
+
+        // 1. Primitive Data Types
+
+        // a) Byte Data Type(-128 to 127)(8-bit)
+        byte c = 127;
+        System.out.println("Byte = " + c);
+
+        // b) Float Data Type(32-bit)
+        float y = 8.0f;
+        System.out.println("Float = " + y);
+
+        // c) Integer Data Type(-2**31 to 2**31-1)(32-bit)
+        int b = 7;
+        System.out.println("Integer = " + b);
+
+        // d) Boolean Data Type(true or false)
+        boolean isVegitarian = true;
+        System.out.println("Boolean = " + isVegitarian);
+
+        // e) Short Data Type(-32768 to 32767)(16-bit)
+        short temperature = -200;
+        System.out.println("Short = " + temperature);
+
+        // e) Long Data Type(-2**63 to 2**63-1)(64-bit)
+        long range = -42332200000L;
+        System.out.println("Long = " + range); 
+
+        // f) Double Data Type(64-bit)
+        double number = -42.3;
+        System.out.println("Double = " + number);
+
+        // g) Char Data Type(16-bit)
+        char letter = 'A';
+        System.out.println("Char = " + letter);
+
+
+        // String Data Type
+        String wishes1 = "Good Morning!";
+        String wishes2 = " Everyone";
+        System.out.println("String = " + wishes1 + wishes2);
+    }
+}
